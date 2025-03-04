4e611cde4faf457a2fc121ff4acc66c042875e40
Bouafif Rayen <rayen.bouafif@insat.ucar.tn>
2025-03-04 15:11:23 +0100

    Fixed Bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,8 +50,7 @@ const sr = ScrollReveal({
     delay: 200,
 //     reset: true
 });
-
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text , .skills__category-title',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img ,.skills__icons',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
